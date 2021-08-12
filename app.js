c3ee8cced34129337f2e7602013a01a495b19fcf
tal <hallroibeard@gmail.com>
2021-08-12 15:58:47 +0200

    Changing live version via circleci and git

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const http = require('http');
 
 http.createServer(function (req, res) {
   res.write('Hello World!');
+  res.write('This line added via Git push.');
   res.end();
 }).listen(8080, '0.0.0.0');
